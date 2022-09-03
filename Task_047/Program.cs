17e6d679599d5eb709b791461db7c4676e4dd274
katerina <you-like-me@yandex.ru>
2022-09-03 15:55:51 +0300

    Add Task 047

---
+++ Task_047/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.Write("Введите количество строк в матрице: m ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов в матрице: n ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+double[,] array = new double[m,n];
+
+void FillArrayDouble(double[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i,j] = Math.Round(new Random().Next(-10, 10) + new Random().NextDouble(), 2);
+        }
+    }
+}
+
+void PrintArrayDouble(double[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+FillArrayDouble(array);
+PrintArrayDouble(array);
+Console.WriteLine();
